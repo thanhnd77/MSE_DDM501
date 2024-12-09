af67e75a84093b769b77d3945f5fa08026cb6732
thanhnd77 <thanh26042001@gmail.com>
2024-12-10 03:23:50 +0700

    add Dockerfile

--- app.py
+++ app.py
@@ -1,73 +1,12 @@
-# from flask import Flask, request, jsonify, render_template
-# import mlflow.sklearn
-# import numpy as np
-# import os
-#
-# mlflow.set_tracking_uri(uri="http://127.0.0.1:7000")
-#
-# app = Flask(__name__)
-#
-# # Load the best model
-# model = mlflow.sklearn.load_model("models:/best_model/latest")
-#
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-#
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.is_json:
-#         data = request.json['data']
-#     else:
-#         data = [float(request.form[f'feature_{i}']) for i in range(20)]
-#
-#     features = np.array(data).reshape(1, -1)
-#     prediction = model.predict(features)
-#     probability = model.predict_proba(features)
-#
-#     result = {
-#         'prediction': int(prediction[0]),
-#         'probability': {
-#             'class_0': float(probability[0][0]),
-#             'class_1': float(probability[0][1])
-#         }
-#     }
-#
-#     if request.is_json:
-#         return jsonify(result)
-#     else:
-#         return render_template('index.html', result=result)
-#
-#
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
-
-
-
-
 from flask import Flask, render_template, request
 import mlflow
 import numpy as np
 
 app = Flask(__name__)
 
-mlflow.set_tracking_uri(uri="http://host.docker.internal:7000")
+# mlflow.set_tracking_uri(uri="http://host.docker.internal:7000")
+mlflow.set_tracking_uri(uri="http://127.0.0.1:7000")
 model = mlflow.sklearn.load_model("models:/best_model/latest")
-# # Load the best model
-# def load_best_model():
-#     client = mlflow.tracking.MlflowClient()
-#     experiment = client.get_experiment_by_name("classification_experiment")
-#     runs = client.search_runs(
-#         experiment_ids=[experiment.experiment_id],
-#         order_by=["metrics.accuracy DESC"]
-#     )
-#     best_run = runs[0]
-#     model_uri = f"runs:/{best_run.info.run_id}/model"
-#     model = mlflow.sklearn.load_model(model_uri)
-#     return model
-#
-#
-# model = load_best_model()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -94,4 +33,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
